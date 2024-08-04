fb50073073523a8342900cf5e52a718c2ec9decc
AlixBarreaux <alix@github.com>
2024-08-04 17:53:31 +0200

    Feat: Check for 2 cells alignments
    If PlayerAI owns 2 cells it'll win on the next turn.

--- scripts/player_ai.gd
+++ scripts/player_ai.gd
@@ -1,10 +1,15 @@
 extends Node
 class_name PlayerAI
 
+### It directly clicks on a ButtonCell in the UI
+### just like the player except it can bypass the "disabled" mode of the UI.
 
 @onready var board: Board = %Board
 
 var player_id: int = 2
+var opponent_player_id: int = 1
+
+var is_game_over = false
 
 
 func check_if_its_turn_to_play() -> void:
@@ -20,20 +25,14 @@ func on_board_assigned_cell() -> void:
 	# If Game mode singleplayer, AI won't do anything.
 	if Global.game_mode == EnumGameModes.GameModes.Multiplayer:
 		return
-	
-	print("Board assigned cell! Global id: ", Global.current_player_id, " VS player_id: ", player_id)
-	
+	#print("Board assigned cell! Global id: ", Global.current_player_id, " VS player_id: ", player_id)
 	self.check_if_its_turn_to_play()
 
 
-var is_game_over = false
-
-
 func on_new_game_started() -> void:
-	print(self.name, ": Game restarted, reset game over to false!")
+	#print(self.name, ": Game restarted, reset game over to false!")
 	is_game_over = false
-	
-	print("Game started! Global id: ", Global.current_player_id, " VS player_id: ", player_id)
+	#print("Game started! Global id: ", Global.current_player_id, " VS player_id: ", player_id)
 	self.check_if_its_turn_to_play()
 
 
@@ -55,7 +54,7 @@ func _ready() -> void:
 
 
 func pick_cell(idx: int) -> void:
-	print(self.name, ": CLICK!")
+	#print(self.name, ": CLICK!")
 	board.click_button_cell(idx)
 
 
@@ -69,7 +68,6 @@ func pick_random_cell() -> void:
 			else:
 				random_cell_index += 1
 			continue
-		
 		pick_cell(random_cell_index)
 		break
 
@@ -82,14 +80,88 @@ func form_two_cells_alignment() -> void:
 	pass
 
 
+func complete_three_cells_alignment() -> void:
+	pass
+
+
 func play_turn() -> void:
-	print(self.name, ": Play turn!")
-	self.pick_random_cell()
+	if Global.turn_counter == 1:
+		print("First turn! Pick a random cell")
+		self.pick_random_cell()
+	else:
+		# Can I win the next turn?
+		print(board.cells)
+		
+		var board_line_to_check: Array = []
+		
+		
+		for winning_line_array in BoardUtils.get_winning_cell_lines():
+			#print(line_to_check, " VS ", winning_line_array)
+			
+			print("\nWinning array: ", winning_line_array)
+			
+			for winning_cell_index in winning_line_array:
+				board_line_to_check.append(board.cells[winning_cell_index])
+			
+			print("Constructed array: ", board_line_to_check)
+			
+			
+			var iterations_count: int = 0
+			var neutral_cell_index: int = 2
+			var neutral_cell_detected: bool = false
+			var owned_cell_detected: bool = false
+			
+			for cell_to_check in board_line_to_check:
+				iterations_count += 1
+				print("Iterations count: ", iterations_count)
+				
+				print("board.cells[] index: ", cell_to_check, " - and value: ", cell_to_check)
+				
+				if cell_to_check == opponent_player_id:
+					print("Opponent already has a cell: ", cell_to_check)
+					break
+				
+				elif cell_to_check == EnumCellOwners.CellOwners.NEUTRAL:
+					if neutral_cell_detected:
+						print("2 neutral cells were detected! Cell: ", cell_to_check)
+						break
+					neutral_cell_index = iterations_count - 1
+					neutral_cell_detected = true
+					continue
+				else:
+					if not owned_cell_detected:
+						print("One owned cell detected!")
+						owned_cell_detected = true
+						continue
+					print("2 owned cells detected!")
+					
+					print("+++++++++++++++ I have an alignment of 2!")
+					print("+++ Combination: ", winning_line_array)
+					print("+++ Owned cells in this combination: ", board_line_to_check)
+					print("+++ I must take the neutral cell: ", neutral_cell_index)
+					
+					print("+++ The neutral cell in the combination is 0: ", board_line_to_check[neutral_cell_index])
+					print("+++ The neutral cell index in the combination is: ", winning_line_array[neutral_cell_index])
+					
+					var final_board_index: int = winning_line_array[neutral_cell_index]
+					print("+++ Index to use in board.cells: ", final_board_index)
+					
+					print("+++ Taking neutral cell in board at index: ", board.cells[final_board_index])
+					pick_cell(final_board_index)
+					return
+				
+			print("Can't find 2 of my cells cells aligned in this line.")
+			
+			board_line_to_check = []
+		print("I can't find any way to win.")
+		pick_random_cell()
+
+## TODO: REUSE THIS 2 ALIGNMENT CHECK TO USE FOR BOTH WINNING AND COUNTERING
+
+
+# Should I counter my opponent?
+# Align
 	
-	#if Global.turn_counter == 1:
-		#self.pick_random_cell()
-	#elif Global.turn_counter == 2:
-		#pass
 	
 	# TODO: SHARE CODE BETWEEN PlayerAI and Board!
 	# Detect opponent's alignment
